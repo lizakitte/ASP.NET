7386eeadc82020773a5e78c690f1cd56c7f934cd
lizakitte <lizakitte@gmail.com>
2023-10-18 14:26:41 +0200

    lab3

---
+++ lab3/Controllers/ContactController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab3_App.Models;
+
+namespace lab3_App.Controllers
+{
+    public class ContactController : Controller
+    {
+        static readonly Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        static int id = 1;
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+    
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model) 
+        { 
+            if (ModelState.IsValid)
+            {
+                model.Id = id++;
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact model)
+        {
+            _contacts.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_contacts[id]);
+        }
+        [HttpPost]
+        public IActionResult Details(Contact model)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ lab3/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab3_App.Models
+{
+    public class Contact
+    {
+    [HiddenInput]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Musisz podac imie")]
+    [StringLength(maximumLength: 50, ErrorMessage = "Zbyt dlugie imie, podaj mniejsze")]
+    public string Name { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    [Phone]
+    public string Phone { get; set; }
+    public DateTime? Birth { get; set; }
+    }
+}
