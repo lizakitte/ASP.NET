f62285b6964fb290020d711badf33dccb9fbbcac
lizakitte <lizakitte@gmail.com>
2023-10-24 19:53:34 +0200

    lab3_car

---
+++ lab3_car/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab3_car.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
